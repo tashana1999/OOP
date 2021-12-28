45af481ad50dd757e9d9f06df55431ea81836d77
tashana1999 <85959669+tashana1999@users.noreply.github.com>
2021-12-28 15:04:13 +0530

    Add files via upload

---
+++ TestCP.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poly;
+
+/**
+ *
+ * @author tashana
+ */
+public class TestCP {
+    
+    static int method(int x, int y){
+          return x+y;
+     }
+
+    static int method(int x){
+          return x;
+     }
+     
+}
+
---
+++ main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poly;
+
+import static com.mycompany.poly.TestCP.method;
+
+/**
+ *
+ * @author tashana
+ */
+public class main {
+public static void main(String[] args){
+        System.out.println(method(5));          //  5
+
+         System.out.println(method(2,4));      //  6
+      }     
+    
+}
